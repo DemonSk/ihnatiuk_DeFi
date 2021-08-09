bd1fd6ff746729f5419524b5a67c9d9ac9c5206a
Serhii Ihnatiuk <ihnatiuksv@gmail.com>
2021-08-09 09:49:02 +0300

    Change to 1m tokens

--- contracts/DSToken.sol
+++ contracts/DSToken.sol
@@ -3,10 +3,9 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract DSToken is ERC20, ERC20Burnable{
+contract DSToken is ERC20{
     constructor() ERC20("Decentralized Systems", "DS") {
-        _mint(msg.sender, 1000);
+        _mint(msg.sender, 1000000000000000000000000);
     }   
 }
